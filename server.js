d825203e3ccc6491e44b3a2cd130afec0df1bfa2
Vicki Shaw <vickishaw@Vickis-MacBook.local>
2017-05-10 19:09:13 -0400

    working mardown editor

--- server.js
+++ server.js
@@ -17,6 +17,23 @@ app.get('/', function(req, res) {
     res.render('pad');
 });
 
+//routes for app
+app.get('/:id)', function(req, res) {
+    res.render('pad');
+});
+
+//get sharejs dependencies
+var sharejs = require('share');
+require('redis');
+
+//options for sharejs
+var options = {
+    db: {type: 'redis'},
+};
+
+//attach express server to sharejs
+sharejs.server.attach(app.options);
+
 //listen on port 8000 (for localhost) or port defined for heroku
 var port = process.env.PORT || 8000;
 app.listen(port);
